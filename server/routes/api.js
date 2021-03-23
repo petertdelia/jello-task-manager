6f0528105c059d6fecfef6d449b3244382118c7e
nlicalzi <nicholas.licalzi@gmail.com>
2021-03-23 12:29:58 -0400

    added new GET route to /boards/:id

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,7 +5,8 @@ const boardsController = require('../controllers/boardsController');
 const { validateBoard } = require('../validators/validators');
 
 router.get('/boards', boardsController.getBoards);
-
 router.post('/boards', validateBoard, boardsController.createBoard);
+
+router.get('/boards/:id', boardsController.getBoard);
 
 module.exports = router;
